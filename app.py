aa4351e31684d0e26cab2bee61dd9d3cf0e112c7
BaileyLeeder <baileyleeder7@gmail.com>
2024-12-09 14:08:01 -0800

    Update app.py bailey

--- app.py
+++ app.py
@@ -87,6 +87,34 @@ def get_data():
     
     return jsonify(InfoDb)
 
+#Bailey API 
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# initialize a flask application (app)
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
+
+# ... your existing Flask
+
+# add an api endpoint to flask app
+@app.route('/api/Bailey/')
+def get_data():
+    # start a list, to be used like a information database
+    InfoDb = []
+
+    # add a row to list, an Info record
+    InfoDb.append({
+        "FirstName": "Bailey",
+        "LastName": "Leeder",
+        "DOB": "June 7",
+        "Residence": "San Diego",
+        "Email": "baileyleeder101@gmail.com",
+        "Owns_Cars": ["Idk a big red one"]
+    })
+    
+    return jsonify(InfoDb)
+
 # add an HTML endpoint to flask app
 @app.route('/')
 def say_hello():

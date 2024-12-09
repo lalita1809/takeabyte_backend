c2b2e5cb19551376c3e76120504e8db7175c9244
Joannahu123 <jomojo8326@gmail.com>
2024-12-09 14:06:05 -0800

    adding my api

--- app.py
+++ app.py
@@ -55,6 +55,10 @@ if __name__ == '__main__':
     # starts flask server on default port, http://127.0.0.1:5001
     app.run(port=5001)
     
+
+
+
+
 #Joanna API 
 from flask import Flask, jsonify
 from flask_cors import CORS
@@ -66,29 +70,19 @@ CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
 # ... your existing Flask
 
 # add an api endpoint to flask app
-@app.route('/api/')
+@app.route('/api/Joanna/')
 def get_data():
     # start a list, to be used like a information database
     InfoDb = []
 
     # add a row to list, an Info record
     InfoDb.append({
-        "FirstName": "John",
-        "LastName": "Mortensen",
-        "DOB": "October 21",
+        "FirstName": "Joanna",
+        "LastName": "Hu",
+        "DOB": "April 26",
         "Residence": "San Diego",
-        "Email": "jmortensen@powayusd.com",
-        "Owns_Cars": ["2015-Fusion", "2011-Ranger", "2003-Excursion", "1997-F350", "1969-Cadillac"]
-    })
-
-    # add a row to list, an Info record
-    InfoDb.append({
-        "FirstName": "Shane",
-        "LastName": "Lopez",
-        "DOB": "February 27",
-        "Residence": "San Diego",
-        "Email": "slopez@powayusd.com",
-        "Owns_Cars": ["2021-Insight"]
+        "Email": "joanna.y.hu@gmail.com",
+        "Owns_Cars": ["2003 Honda accord"]
     })
     
     return jsonify(InfoDb)
@@ -111,6 +105,8 @@ def say_hello():
 if __name__ == '__main__': 
     # starts flask server on default port, http://127.0.0.1:5001
     app.run(port=5001)
+
+
 
 
 
